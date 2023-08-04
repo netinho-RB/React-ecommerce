6d8dae44b4d915c7bd052c8d93c77be1d637da45
Nestor Nunes <109300513+netinho-RB@users.noreply.github.com>
2023-08-04 15:01:00 -0300

    subinnnndo

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,25 +1,26 @@
-import React from 'react';
+// src/components/ProductDetails.js
 
-function ProductDetails() {
-  // Aqui você pode receber os detalhes da fruta clicada
-  // Exemplo de como exibir os detalhes:
-  const fruit = {
-    id: 1,
-    name: 'Banana',
-    price: 2.5,
-    description: 'Uma fruta deliciosa e nutritiva.',
-    image: 'banana.jpg',
-  };
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import frutasData from '../data/frutas.json';
+
+const ProductDetails = () => {
+  const { id } = useParams();
+  const fruta = frutasData.find((fruta) => fruta.id === parseInt(id));
 
   return (
-    <div className="product-details">
-      <img src={`images/${fruit.image}`} alt={fruit.name} />
-      <h2>{fruit.name}</h2>
-      <p>Preço: R${fruit.price.toFixed(2)}</p>
-      <p>{fruit.description}</p>
-      <button>Adicionar ao Carrinho</button>
+    <div>
+      {fruta ? (
+        <div>
+          <h2>{fruta.nome}</h2>
+          <p>Price: {fruta.preco}</p>
+          <p>Description: {fruta.descricao}</p>
+        </div>
+      ) : (
+        <p>Product not found!</p>
+      )}
     </div>
   );
-}
+};
 
 export default ProductDetails;
